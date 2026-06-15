829774230f63108406a7c70920211cc1a405b83f
Abdiramadhan096 <mohabdi241@gmail.com>
2026-06-15 23:10:48 +0800

    Membuat Kategori

--- project_padi.py
+++ project_padi.py
@@ -37,3 +37,25 @@ df = df.dropna()
 
 # Menghapus baris total provinsi jika ada
 df = df[df["kabupaten_kota"].str.lower() != "sulawesi tengah"]
+
+# 5. Membuat kategori produksi untuk SVC
+batas_rendah = df["produksi_padi_ton"].quantile(1/3)
+batas_tinggi = df["produksi_padi_ton"].quantile(2/3)
+
+def buat_kategori(nilai):
+    if nilai <= batas_rendah:
+        return "Rendah"
+    elif nilai <= batas_tinggi:
+        return "Sedang"
+    else:
+        return "Tinggi"
+
+df["kategori_produksi"] = df["produksi_padi_ton"].apply(buat_kategori)
+
+print("\nBatas kategori produksi:")
+print(f"Rendah  : <= {batas_rendah:.2f} ton")
+print(f"Sedang  : > {batas_rendah:.2f} sampai <= {batas_tinggi:.2f} ton")
+print(f"Tinggi  : > {batas_tinggi:.2f} ton")
+
+print("\nJumlah kategori:")
+print(df["kategori_produksi"].value_counts())
