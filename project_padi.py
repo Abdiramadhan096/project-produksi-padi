044cae19a65e0ae3d5a388c5d65fcd5a67231b7e
Abdiramadhan096 <mohabdi241@gmail.com>
2026-06-15 23:56:49 +0800

    Memperbaiki Library

--- project_padi.py
+++ project_padi.py
@@ -193,3 +193,19 @@ plt.title("Tren Total Produksi Padi Sulawesi Tengah 2018-2025")
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+# 1. Import library
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVC
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score, classification_report
